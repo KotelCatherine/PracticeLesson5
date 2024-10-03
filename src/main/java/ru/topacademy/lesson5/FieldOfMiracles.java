da00f951a50a6b8ca104bd52b4d75a1b9f54a767
CotelCatherine <bezmozggoff@gmail.com>
2024-10-03 15:18:59 +0700

    Задачи с массивом и Поле чудес

---
+++ src/main/java/ru/topacademy/lesson5/FieldOfMiracles.java
@@ -0,0 +1,73 @@
+package ru.topacademy.lesson5;
+
+import java.util.Scanner;
+
+
+public class FieldOfMiracles {
+    Scanner scanner = new Scanner(System.in);
+    private String[] arrList = {"lucky", "storm", "beard"};
+    private int numHiddenWord = (int) (Math.random() * 3);
+    private String hiddenWord = arrList[numHiddenWord];
+    private int hiddenWordLength = hiddenWord.length();
+    private String[] hiddenLetterList = new String[hiddenWordLength];
+    int attempt = 5;
+
+    public FieldOfMiracles() {
+
+        createField(hiddenLetterList);
+        startGame(attempt);
+    }
+
+    private void createField(String[] hiddenWordListChar) {
+        for (int i = 0; i < hiddenWordListChar.length; i++) {
+            hiddenWordListChar[i] = String.valueOf('_');
+        }
+    }
+
+    private void startGame(int attempt) {
+        System.out.println("Угадайте слово по буквам, у Вас есть " + attempt + " попыток. Длина слова " +
+                hiddenWordLength +
+                "\nВведите букву");
+
+        while (hiddenWordLength != 0 || attempt != 0) {
+            char hiddenLetter = scanner.next().charAt(0);
+
+            for (int i = 0; i < hiddenWord.length(); i++) {
+                if (guessedTheLetter(hiddenLetter, hiddenWord.charAt(i), i)) {
+                    hiddenWordLength--;
+                } else {
+                    attempt--;
+                    System.out.println("Не угадали. Попробуйте снова. У вас осталось " + attempt + " попыток.");
+                }
+            }
+            if (attempt == 0) {
+                System.out.println("У Вас закончились попытки.");
+            }
+            if (hiddenWordLength == 0) {
+                System.out.println("Поздравляю Вы угадали слово!");
+            }
+        }
+    }
+
+    private boolean guessedTheLetter(char hiddenLetter, char c, int i) {
+        boolean isGuess = false;
+
+        if (hiddenLetter == c) {
+            System.out.println("Вы угадали " + (i + 1) + " букву");
+            isGuess = true;
+        }
+        if (isGuess) {
+            hiddenLetterList[i] = String.valueOf(hiddenLetter);
+            fillingTheField(hiddenLetterList);
+            System.out.println();
+        }
+        return isGuess;
+    }
+
+
+    private void fillingTheField(String[] hiddenWordListChar) {
+        for (String s : hiddenWordListChar) {
+            System.out.print(s);
+        }
+    }
+}
